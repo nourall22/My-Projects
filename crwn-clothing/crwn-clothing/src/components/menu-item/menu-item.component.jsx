5a2193aede0cfb409e8cee0684b0c2761936f9eb
nourall22 <nidorigin@gmail.com>
2020-10-06 19:35:10 +0100

    crwn-clothing

--- crwn-clothing/crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item-styles.scss";
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item `}>
+
+const MenuItem = ({ title, imageUrl, size, history }) => (
+  <div className={`${size} menu-item `} onClick={() => history.push()}>
     <div
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
+
     <div className="content">
       <h1 className="title"> {title.toUpperCase()} </h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
-export default MenuItem;
+export default withRouter(MenuItem);
